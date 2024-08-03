6009ec2b6ef1b883d3413eeb5c177211c8b17b8a
Vincent <vincentli.dev@gmail.com>
2024-08-03 04:05:25 -0400

    added forum page and routed pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing_Page from "./pages/landing_page";
+import AirQuality from './pages/AirQuality';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Logout from './pages/Logout';
+import Signup from './pages/Signup';
+import Stats from './pages/Stats';
+import Forum from './pages/Forum';
+
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
 	<BrowserRouter>
 	  <Routes>
-		<Route path="/" element={<Landing_Page />}>
+		<Route path="/" element={<Home />}>
+		<Route path="/stats" element={<Stats/>}/>
+		<Route path="/air-quality" element={<AirQuality/>}/>
+		<Route path="/forum" element={<Forum/>}/>
+		<Route path="/login" element={<Login/>}/>
+		<Route path="/logout" element={<Logout/>}/>
+		<Route path="/sign-up" element={<Signup/>}/>
 		  {/* <Route index element={<Home />} />
 		  <Route path="blogs" element={<Blogs />} />
 		  <Route path="contact" element={<Contact />} />
---
+++ src/pages/Forum.js
@@ -0,0 +1,5 @@
+const Forum = () => {
+    return <h1>tdsafsest</h1>;
+  };
+  
+export default Forum;
