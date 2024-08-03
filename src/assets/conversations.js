28051d8a76b6659c9da6a7b4aac6314d4b17a1d6
Vincent <vincentli.dev@gmail.com>
2024-08-03 11:11:18 -0400

    added forum data

---
+++ src/assets/conversations.js
@@ -0,0 +1,26 @@
+export const conversations = [
+  {
+    title: "title",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dolor magna, faucibus at nisi eu, pharetra congue orci. Vestibulum finibus dapibus quam, non sollicitudin nisl cursus feugiat. Aliquam eu arcu a eros facilisis vehicula. Sed eu ullamcorper nisl. Nam maximus consequat luctus. Vestibulum mollis blandit turpis, ac mollis elit ornare sed. In bibendum, nisi at cursus pharetra, odio nulla blandit purus, eget eleifend nisl diam non odio. Etiam quis purus sem. Vivamus ultricies scelerisque massa id interdum. In hendrerit porta tincidunt. Nunc euismod ligula quis ante sollicitudin, vitae posuere erat ullamcorper. Quisque luctus gravida massa a ullamcorper. Vestibulum vestibulum mi semper vulputate rutrum.",
+    up: 7,
+    down: 1,
+    comment: 4,
+    id: 1,
+  },
+  {
+    title: "title",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dolor magna, faucibus at nisi eu, pharetra congue orci. Vestibulum finibus dapibus quam, non sollicitudin nisl cursus feugiat. Aliquam eu arcu a eros facilisis vehicula. Sed eu ullamcorper nisl. Nam maximus consequat luctus. Vestibulum mollis blandit turpis, ac mollis elit ornare sed. In bibendum, nisi at cursus pharetra, odio nulla blandit purus, eget eleifend nisl diam non odio. Etiam quis purus sem. Vivamus ultricies scelerisque massa id interdum. In hendrerit porta tincidunt. Nunc euismod ligula quis ante sollicitudin, vitae posuere erat ullamcorper. Quisque luctus gravida massa a ullamcorper. Vestibulum vestibulum mi semper vulputate rutrum.",
+    up: 3,
+    down: 3,
+    comment: 8,
+    id: 2,
+  },
+  {
+    title: "title",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dolor magna, faucibus at nisi eu, pharetra congue orci. Vestibulum finibus dapibus quam, non sollicitudin nisl cursus feugiat. Aliquam eu arcu a eros facilisis vehicula. Sed eu ullamcorper nisl. Nam maximus consequat luctus. Vestibulum mollis blandit turpis, ac mollis elit ornare sed. In bibendum, nisi at cursus pharetra, odio nulla blandit purus, eget eleifend nisl diam non odio. Etiam quis purus sem. Vivamus ultricies scelerisque massa id interdum. In hendrerit porta tincidunt. Nunc euismod ligula quis ante sollicitudin, vitae posuere erat ullamcorper. Quisque luctus gravida massa a ullamcorper. Vestibulum vestibulum mi semper vulputate rutrum.",
+    up: 12,
+    down: 4,
+    comment: 6,
+    id: 3,
+  },
+];
--- src/pages/Forum.js
+++ src/pages/Forum.js
@@ -1,7 +1,42 @@
+import Conversation from "../components/Conversation";
+import { conversations } from "../assets/conversations";
 const Forum = () => {
   return (
-    <div className="flex flex-wrap justify-center space-x-5 border-2 rounded-lg m-5 p-5">
-      <h1>tdsafsest</h1>;
+    <div>
+      <div className="ml-6">
+        <button className="flex">
+          New post
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            aria-hidden="true"
+            className="w-6 h-6"
+            data-slot="icon"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
+            />
+          </svg>
+        </button>
+      </div>
+
+      {conversations.map((conv, ind) => (
+        <div className="flex flex-col space-x-5 border-2 rounded-lg m-5 p-5">
+          <Conversation
+            id={ind}
+            title={conv.title}
+            body={conv.body}
+            up={conv.up}
+            down={conv.down}
+            comment={conv.comment}
+          />
+        </div>
+      ))}
     </div>
   );
 };
