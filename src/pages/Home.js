ab131326bee4b3bbbb4b4e9881ce3aefe0d5b8e6
Vincent <vincentli.dev@gmail.com>
2024-08-04 01:59:59 -0400

    saving current progress on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 import Login from "./Login";
 import { Link } from "react-router-dom";
+import MostRecentActivity from "../components/MostRecentActivity";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -14,7 +15,16 @@ const Home = () => {
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="">PUT NEWS STUFF HERE</div>
-      <div>PUT RECENT ACTIVITY HERE (transit, health app, purchases)</div>
+      <div>
+        <MostRecentActivity />
+        <Link
+          to="/recent_activity"
+          className="border-b border-b-blue-400 text-blue-400"
+        >
+          View More
+        </Link>
+      </div>
+
       <div className="fixed bottom-16 my-8 w-80 p-4 rounded-xl bg-[#79B859] justify-center items-center">
         <Link
           className="text-lg flex justify-center items-center text-[#484D6D]"
