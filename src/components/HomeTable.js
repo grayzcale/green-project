f188db1de52bf5ef1b15522ec5203c3c1cd5aa7d
grayzcale <00xima16@gmail.com>
2024-08-04 08:39:00 -0400

    Add recent acitivty component

--- src/components/HomeTable.js
+++ src/components/HomeTable.js
@@ -1,44 +1,37 @@
 const HomeTable = () => {
   return (
-    <div className="pb-20">
-      <div className="w-full">
-        <table className="w-full">
-          <caption className="p-3 font-bold text-lg">Recent Activity</caption>
-          <thead className="bg-slate-50">
-            <tr>
-              <th className="text-left text-xs p-2">Activity</th>
-              <th className="text-center text-xs p-2" style={{ width: "30%" }}>
-                Points Earned
-              </th>
-            </tr>
-          </thead>
-          <tbody className="text-sm">
-            <tr className="bg-slate-200" style={{ backgroundColor: "#bfdba7" }}>
-              <td className="text-left p-2">
-                Took a train to Toronto on August 4th
-              </td>
-              <td className="text-center p-2">32</td>
-            </tr>
-            <tr className="bg-slate-50">
-              <td className="text-left p-2">Ran 6.7 km on August 1st</td>
-              <td className="text-center p-2">7</td>
-            </tr>
-            <tr className="bg-slate-200" style={{ backgroundColor: "#bfdba7" }}>
-              <td className="text-left p-2">
-                Auto-loaded your Presto Card for $100
-              </td>
-              <td className="text-center p-2">42</td>
-            </tr>
-            <tr className="bg-slate-50">
-              <td className="text-left p-2">...</td>
-              <td className="text-center p-2">...</td>
-            </tr>
-            <tr className="bg-slate-200" style={{ backgroundColor: "#bfdba7" }}>
-              <td className="text-left p-2">...</td>
-              <td className="text-center p-2">...</td>
-            </tr>
-          </tbody>
-        </table>
+    <div className="">
+      <div className="p-4 text-center">
+        <span className="pb-4 font-bold text-lg mx-auto">Recent Activity</span>
+        <div className="bg-gray-100 p-2 mt-2 px-4 grid grid-cols-2 rounded-lg shadow-md">
+          <span className="inline-block text-left border-r-black">Activity</span>
+          <span className="inline-block text-right border-l-2 border-l-gray-300">Points Earned</span>
+        </div>
+
+        <div className="bg-indigo-50 p-2 px-5 my-2 grid grid-cols-2 rounded-lg text-sm font-semibold shadow-md">
+          <span className="inline-block text-left border-r-black">Took a train to Toronto on August 4th</span>
+          <span className="inline-block text-right">18</span>
+        </div>
+
+        <div className="bg-gray-100 p-2 px-5 my-2 grid grid-cols-2 rounded-lg text-sm font-semibold shadow-md">
+          <span className="inline-block text-left border-r-black">Ran 6.7 km on August 1st</span>
+          <span className="inline-block text-right">7</span>
+        </div>
+
+        <div className="bg-indigo-50 p-2 px-5 my-2 grid grid-cols-2 rounded-lg text-sm font-semibold shadow-md">
+          <span className="inline-block text-left border-r-black">Auto-loaded your Presto Card for $100</span>
+          <span className="inline-block text-right">42</span>
+        </div>
+
+        <div className="bg-gray-100 p-2 px-5 my-2 grid grid-cols-2 rounded-lg text-sm font-semibold shadow-md">
+          <span className="inline-block text-left border-r-black">...</span>
+          <span className="inline-block text-right">...</span>
+        </div>
+
+        <div className="bg-indigo-50 p-2 px-5 my-2 grid grid-cols-2 rounded-lg text-sm font-semibold shadow-md">
+          <span className="inline-block text-left border-r-black">...</span>
+          <span className="inline-block text-right">...</span>
+        </div>
       </div>
     </div>
   );
