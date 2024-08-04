770c572875687625891a60054199078fad6bd35d
grayzcale <00xima16@gmail.com>
2024-08-04 02:56:33 -0400

    Change button color

--- src/pages/AI.js
+++ src/pages/AI.js
@@ -4,7 +4,7 @@ export default () => {
             <div className="fixed bottom-24 left-0 right-0 rounded-lg shadow-md h-14 mx-3 content-center inline-flex justify-between">
                 <input className="inline-block m-3 w-full" placeholder="Ask a question..."></input>
                 <button className="inline-block p-2 rounded-lg">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#000000" className="size-8"><path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" /></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#555555" className="size-8"><path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" /></svg>
                 </button>
             </div>
             
