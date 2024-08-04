6f99c04537410e0ee4e6602c43fabb62260d1262
Vincent <vincentli.dev@gmail.com>
2024-08-04 00:29:56 -0400

    removed legend

--- src/pages/AirQuality.js
+++ src/pages/AirQuality.js
@@ -1,5 +1,5 @@
 //import Leaderboard from "../components/Leaderboard";
-import Legend from "../components/Legend";
+// import Legend from "../components/Legend";
 // import Tips from "../components/Tips";
 const AirQuality = () => {
   return (
@@ -12,7 +12,7 @@ const AirQuality = () => {
       <span className="py-2 text-center block mx-auto bg-orange-500 font-bold text-lg text-white shadow-md">
         SENSITIVE
       </span>
-      <Legend />
+      {/* <Legend /> */}
       <div className="flex items-center justify-center mx-6">
         <iframe className="w-full" src="https://waqi.info/#/c/43.702/-79.372/9.8z" title="Air Quality Index" height="500px" scrolling="no"></iframe>
       </div>
