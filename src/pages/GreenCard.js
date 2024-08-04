f809113c8f75acc44879cf7b8b95b6f757fe5d00
Vivillia <vivillia.luo@gmail.com>
2024-08-04 07:38:12 -0400

    update EcoTrack Card details

--- src/pages/GreenCard.js
+++ src/pages/GreenCard.js
@@ -2,8 +2,8 @@ const GreenCard = () => {
   return (
     <div className="flex h-full flex-col justify-center items-center">
       <div className="bg-slate-300 p-3 w-52 rounded-2xl text-center">
-        <span className="text-sm">Carbon Footprint Rating: 300</span>
-        <span className="block text-gray-800 text-2xl font-bold">10% Cashback</span>
+        <span className="text-sm">EcoTrack Score: 82%</span>
+        <span className="block text-gray-800 text-2xl font-bold">2x Points</span>
       </div>
       <img
         src="images/greencard.jpg"
@@ -11,7 +11,7 @@ const GreenCard = () => {
         alt="green card"
       />
       <div className="flex">
-        <span className="">Member since: 2022</span>
+        <span className="">Member since: 2024</span>
       </div>
       <span className="font-bold text-lg">4444 1234 9182 23</span>
     </div>
